c237d93cfba3d978eca0360336dce14bc2dece31
Tran Ngoc Tan Dat <21130314@st.hcmuaf.edu.vn>
2024-05-28 22:03:43 +0700

    finished footer

--- fe/shopgame/src/App.test.tsx
+++ fe/shopgame/src/App.test.tsx
@@ -1,8 +1,13 @@
+import React from 'react';
+// @ts-ignore
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// @ts-ignore
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
+  // @ts-ignore
   expect(linkElement).toBeInTheDocument();
 });
+
---
+++ fe/shopgame/src/pages/users/theme/masterLayout/index.tsx
@@ -0,0 +1,22 @@
+
+import React, {ReactNode} from 'react'
+import Header from '../header/indexHeader'
+import Footer from '../footer/indexFooter'
+import HomePage from '../../homePage/indexHome'
+
+interface MasterLayoutProps {
+    children: ReactNode;
+    [key: string]: any; // Kiểu cho các prop bổ sung khác
+}
+
+const MasterLayout: React.FC<MasterLayoutProps> = ({ children, ...props }) => {
+    return(
+        <div>
+        <Header/>
+       {children}
+        <Footer/>
+        </div>
+    )
+}
+
+export default MasterLayout
