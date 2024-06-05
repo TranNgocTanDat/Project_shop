186489f58243adf8150da44325faad03ae6eaf31
DanhNguyen1809 <21130308@st.hcmuaf.edu.vn>
2024-06-05 19:23:39 +0700

    test

--- fe/shopgame/src/component/user/checkLogin.ts
+++
@@ -1,6 +0,0 @@
-import users from "./dataUser";
-
-export const check = (userName: string, passWord: string): boolean => {
-    const user = users.find(user => user.userName === userName && user.passWord === passWord);
-    return !!user;
-}
