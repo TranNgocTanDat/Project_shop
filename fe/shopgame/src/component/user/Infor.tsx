db62b699e91b6e18a7b5d1b874137283d15cc7c6
DanhNguyen1809 <21130308@st.hcmuaf.edu.vn>
2024-06-09 00:32:30 +0700

    update user

--- fe/shopgame/src/component/user/Infor.tsx
+++ fe/shopgame/src/component/user/Infor.tsx
@@ -1,9 +1,37 @@
 import React from "react";
+import {DataUser} from "./dataUser";
+import './infor.css';
 
-const Infor = () => {
+
+
+interface UserItem{
+    id: number,
+    userName: string,
+    passWord: string,
+}
+
+interface InforProps {
+    user: DataUser;
+}
+
+
+const Infor: React.FC<InforProps> = ({user}) => {
+
     return(
-        <div>
-            hello
+        <div className="infor">
+            <div className="infor__left">
+                <img className="infor__left--avt" src={user.avt}/>
+                {/*<button onClick={}*/}
+            </div>
+            <div className="infor__right">
+                <label className="infor__right--title">Thông Tin cá Nhân</label>
+                <div className="infor__id">ID: {user.id}</div>
+                <div className="infor__userName">User Name: {user.userName}</div>
+                <div className="infor__mail">Mail: {user.mail}</div>
+                <div className="infor__phone">Phone: {user.phone}</div>
+                <div className="infor__adress">Adress: {user.adress}</div>
+                <div className="infor__cccd">CCCD: {user.cccd}</div>
+            </div>
         </div>
     );
 }
