f7a5f9a8f3bfbfc4f0b811fffae99a0b11dc7deb
DanhNguyen1809 <21130308@st.hcmuaf.edu.vn>
2024-07-15 18:54:10 +0700

    updateCode

--- fe/shopgame/src/component/user/checkLogin.ts
+++ fe/shopgame/src/component/user/checkLogin.ts
@@ -1,4 +1,6 @@
 import users, {DataUser} from "./dataUser";
+import User from "./User";
+import user from "./User";
 
 
 
@@ -10,4 +12,20 @@ export const check = (userName: string, passWord: string): DataUser | null => {
 export const checkID = (userID: { userName: string; passWord: string }): number | null => {
     const user = users.find(user => user.userName === userID.userName && user.passWord === userID.passWord);
     return user ? user.id : null;
+}
+
+export const getBalance = (userName: {userName: string; passWord: string}): number | null =>{
+    const user = check(userName.userName, userName.passWord);
+    if (!user) return null;
+    const balance = user.balance;
+    return balance;
+}
+
+export const updateUser = (users: DataUser, newBalance: number): DataUser | null => {
+    const user = check(users.userName, users.passWord);
+    if(user){
+        user.balance = newBalance;
+        return user;
+    }
+    return null;
 }
