c97de8e5433babe19d3e75e69c942eb925ab1aca
Tran Ngoc Tan Dat <21130314@st.hcmuaf.edu.vn>
2024-07-15 20:25:18 +0700

    up code thanh toan

--- fe/shopgame/src/component/user/User.tsx
+++ fe/shopgame/src/component/user/User.tsx
@@ -1,14 +1,15 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import './User.css'
-import {check, checkID} from "./checkLogin";
+import { check, checkID, getBalance } from "./checkLogin";
 import Infor from "./Infor";
-import {DataUser} from "./dataUser";
+import { DataUser } from "./dataUser";
 
 
 const User: React.FC = () => {
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
     const [user, setUser] = useState<DataUser | null>(null);
+    const [balance, setBalance] = useState<number | null>(null);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [error, setError] = useState('');
 
@@ -16,40 +17,48 @@ const User: React.FC = () => {
     useEffect(() => {
         const storedUser = localStorage.getItem('user');
         if (storedUser) {
-            setUser(JSON.parse(storedUser));
+            const user = JSON.parse(storedUser);
+            setUser(user);
+            const userBalance = getBalance(user);
+            setBalance(userBalance);
         }
     }, []);
+
     const handleLogin = (e: React.FormEvent) => {
         e.preventDefault();
         const isAuthenticated = check(userName, password);
         if (isAuthenticated) {
             setUser(isAuthenticated);
+            const userBalance = getBalance(isAuthenticated);
+            setBalance(userBalance);
             localStorage.setItem('user', JSON.stringify(isAuthenticated));
             setError('');
         } else {
             setUser(null);
+            setBalance(null);
             setError('Invalid username or password');
         }
     };
 
     // đăng xuất
 
-     const handleLogout = () => {
+    const handleLogout = () => {
         setUser(null);
+        setBalance(null);
         setIsLoggedIn(false);
         localStorage.removeItem('user');
         setError('');
     };
 
-    return(
+    return (
         <div>
-            {user? (
+            {user ? (
                 <>
-                    <Infor user={user}/>
+                    <Infor user={user} balance={balance} />
                     <button className="logout" onClick={handleLogout}>Logout</button>
                 </>
 
-            ): (
+            ) : (
                 <div className="user">
                     <label className="user__Tlogin">Login</label>
                     <label className="user__title">Email or USerName</label>
@@ -62,10 +71,10 @@ const User: React.FC = () => {
                     />
                     <label className="user__title user__title--pass">PassWord</label>
                     <input type='password'
-                           placeholder=''
-                           value={password}
-                           className="user__input"
-                           onChange={(e) => setPassword(e.target.value)}
+                        placeholder=''
+                        value={password}
+                        className="user__input"
+                        onChange={(e) => setPassword(e.target.value)}
 
                     />
                     <button onClick={handleLogin} className="user__login">Login</button>
